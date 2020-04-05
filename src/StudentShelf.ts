0fb8d8fd586ad8c60e27235c8cbfe5049d35c81c
wolf <nyan643.ym25mm10@gmail.com>
2020-04-05 16:22:52 +0900

    init: Add student data

---
+++ src/StudentShelf.ts
@@ -0,0 +1,23 @@
+import { Student } from "./Student";
+
+export class StudentShelf{
+  // student data
+  private studentList: Array<Student>;
+
+  constructor(){}
+
+  /**
+   * - Getter : Get student list data .
+   */
+  public getData() {
+    
+  }
+
+  /**
+   * - Setter : Set student list data .
+   */
+  public setData() {
+    
+  }
+
+}
