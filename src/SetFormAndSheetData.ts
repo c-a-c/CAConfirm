e60b9f6c3fe3365b22b8543c1a5a0c83d30af8a6
wolf <nyan643.ym25mm10@gmail.com>
2020-04-09 07:56:13 +0900

    add: Add new trigger. Delete old trigger. to stay unique trigger ...

--- src/SetFormAndSheetData.ts
+++ src/SetFormAndSheetData.ts
@@ -3,14 +3,12 @@ import { Form } from './Form';
 import { Sheet } from './Sheet';
 import { AddDesign } from './AddDesign';
 import { StudentShelf } from './StudentShelf';
-import { Student } from './Student';
 
 export class SetFormAndSheetData {
   private _prop: Property;
   private _form: Form;
   private _sheet: Sheet;
   private _design: AddDesign;
-  private _answeredMenber: { [key: string]: boolean } = {};
   private _sumStudent: number;
   private _studentNumberList: String[];
 
@@ -26,11 +24,42 @@ export class SetFormAndSheetData {
   }
 
   /**
-   * makeSheet
-   * @param
-   * @returns
+   * - Set trigger to launch automatically .
+   *  - set new trigger after deleting old trigger .
    */
-  public makeSheet() {
+  public setTrigger() {
+    this.deleteTrigger();
+    this.makeSheet();
+
+    try {
+      ScriptApp.newTrigger('makeSheet')
+        .forForm(this._form.openApps())
+        .onFormSubmit()
+        .create();
+      Logger.log('successly create trigger .');
+    } catch (error) {
+      Logger.log('Miss: cannot set trigger');
+    }
+  }
+
+  /**
+   * - Set trigger to launch automatically .
+   */
+  private deleteTrigger() {
+    for (const trigger of ScriptApp.getProjectTriggers()) {
+      try {
+        ScriptApp.deleteTrigger(trigger);
+        Logger.log('successly delete trigger .');
+      } catch (error) {
+        Logger.log('Miss: cannot delete trigger .');
+      }
+    }
+  }
+
+  /**
+   * - Connect Form and Sheet data .
+   */
+  private makeSheet() {
     /* Insert new sheet */
     this._sheet.setForEveryoneSheetName(this._prop.getEventName);
     this._sheet.insertSheets(this._prop.getEventName);
@@ -41,20 +70,10 @@ export class SetFormAndSheetData {
     /* Set student data */
     let studentShelf: StudentShelf = new StudentShelf(this._sumStudent);
 
-    /* Get form property */
-    // this._answeredMenber = this._form.getTitle(this._answeredMenber);
     /* Read sheet */
-    this._design = new AddDesign(this._sheet.getMenbersSheet(), this._sheet.getForEveryoneSheet());
+    this._design = new AddDesign(this._sheet.getForEveryoneSheet());
     this._sheet.setStudentName();
     studentShelf = this._sheet.setStudentData(studentShelf);
-
-    // for (let item = 0; item < studentShelf.getMaxIndex(); item++) {
-    //   Logger.log(
-    //     `${item} -> ${studentShelf.getList(item).getName()} ${studentShelf
-    //       .getList(item)
-    //       .getNumber()} : ${studentShelf.getList(item).getFlag()}`
-    //   );
-    // }
 
     /* Get form property */
     this._studentNumberList = this._form.getTitle();
@@ -65,11 +84,11 @@ export class SetFormAndSheetData {
         if (studentShelf.getList(counter).getNumber() == this._studentNumberList[arrayItem]) {
           // Set true flag for student
           studentShelf.getList(counter).setFlag();
-          Logger.log(studentShelf.getList(counter).getNumber());
+          // Logger.log(studentShelf.getList(counter).getNumber());
         }
       }
     }
-    Logger.log(studentShelf.getMaxIndex());
+    // Logger.log(studentShelf.getMaxIndex());
 
     this._design.judgeData(studentShelf);
   }
